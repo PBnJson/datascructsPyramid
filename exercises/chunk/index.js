8a5d4e4665deebe5f3883a8d84e2a10f51f012f2
Joshua Clancy <ukg_123@yahoo.com>
2023-07-22 14:39:22 -0500

    asdf

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -6,8 +6,31 @@
 // chunk([1, 2, 3, 4, 5], 2) --> [[ 1, 2], [3, 4], [5]]
 // chunk([1, 2, 3, 4, 5, 6, 7, 8], 3) --> [[ 1, 2, 3], [4, 5, 6], [7, 8]]
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
-// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
+// chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]'
 
-function chunk(array, size) {}
+// const letters = ['a', 'b', 'c', 'd', 'e'];
+// letters.slice(0, 3) < beginning at el 0 to el 3, get a copy of every one expet the last one. so this would return [a, b, c], so the first 3 elements.
 
+function chunk(array, size) {
+    const emptyChunk = [];
+    let index = 0;
+    while (index < array.length) {
+        emptyChunk.push(array.slice(index, index + size));
+        index += size;
+    }
+    return emptyChunk;
+}
+
+// function chunk(array, size) {
+//     const newNumChunk = []
+//     for (let element of array) {
+//         let lastElement = newNumChunk[newNumChunk.length - 1];
+//         if (!lastElement || lastElement.length === size) {
+//             newNumChunk.push([element]);
+//         } else {
+//             lastElement.push(element);
+//         }
+//     }
+//     return newNumChunk;
+// }
 module.exports = chunk;
